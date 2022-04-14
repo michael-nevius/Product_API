327406b59f4b887272fff46160d4e91882fe3da3
michael-nevius <michael.a.nevius@gmail.com>
2022-04-14 14:19:06 -0400

    Completed post functionality on my reviews app + everything has been tested in postman and is working

--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,4 @@
 from rest_framework.decorators import api_view
-from django.shortcuts import get_object_or_404, get_list_or_404
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import ReviewSerializer
@@ -12,21 +11,21 @@ def reviews_list(request):
     if request.method == 'GET':
         reviews = Review.objects.all()
         serializer = ReviewSerializer(reviews, many=True)
-        return Response(serializer.data, status=200)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-@api_view(['GET','POST','PUT','DELETE'])
+@api_view(['GET','POST'])
 def review_detail(request,pk):
     request.data.update({"product": pk})
 
     if request.method == 'GET':
         reviews = Review.objects.filter(product_id=pk)
         serializer = ReviewSerializer(reviews, many=True)
-        return Response (serializer.data, status=200)
+        return Response (serializer.data, status=status.HTTP_200_OK)
 
     elif request.method == 'POST':
         serializer = ReviewSerializer(data=request.data)
         if serializer.is_valid() == True:
             serializer.save()
-            return Response(serializer.data, status=201)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
-            return Response(serializer.data.product, status=400)
+            return Response(serializer.data.product, status=status.HTTP_404_NOT_FOUND)
